8cc33e16a3cd097857a8fdca4bb18cb492ec43f4
VladBero <glamandir01@gmail.com>
2023-02-15 00:13:55 +0200

    Inialized branch and created "Hello, World!"

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json({type: 'application/json'}))
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
