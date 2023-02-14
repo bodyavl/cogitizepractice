6092b56710ced1d96d36f9678530890fecb2514c
VetsGo <springtrapukrainian@gmail.com>
2023-02-14 14:16:34 +0200

    added bodyParser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json({type: 'application/json'}))
 const port = 3000
 
 app.get('/', (req, res) => {
