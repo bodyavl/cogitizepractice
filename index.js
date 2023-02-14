1e3e34b0d7030d71daaf5509ce7d0c10a8e5b226
Nikita Liberman <kykylib107@gmail.com>
2023-02-14 12:55:58 +0200

    initialized the branch and did the task

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json({ type: 'application/json' }))
+const port = 3000
+
+app.get('/', (req, res) => {
+  console.log(req.query);
+  res.send('Hello World!')
+})
+app.post('/', (req, res) => {
+  console.log(req.body);
+    res.send('Hello World! POST')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
