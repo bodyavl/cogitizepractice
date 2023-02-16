126d193b76f21ed0e2997fc9a6d351ebed8ec179
v1adem <v1adem@ukr.net>
2023-02-17 00:54:19 +0200

    Adding error hadling, formating

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const express = require('express')
-const bodyparser = require('body-parser')
 const app = express()
-app.use(bodyparser.json({ type: "application/json"}))
+
+const bodyparser = require('body-parser')
+app.use(bodyparser.json())
+
+const database = require("./database")
+
 const port = 3000
 
-app.get('/', (req, res) => {
-    console.log(req.query);
-    res.send('Hello Get')
+const errorHandler = (error, req, res, next) => {
+    res.header("Content-Type", "application/json")
+    console.log("Error occured:", error.message);
+    res.status(500).send(error.message)
+}
+
+app.get('/getMovie/:id', async (req, res, next) => {
+    try {
+        const movieId = req.params.id;
+    }
+    catch (error) {
+        next(error)
+    }
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Hello Post')
-  })
+app.get('/movies', async (req, res, next) => {
+    try {
+        const moviesList = req.params.id;
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+app.post('/addMovie', async (req, res, next) => {
+    try {
+        const addMovie = req.params.id;
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Server started on ${port} port`)
 })
