f5fc2f49bc8f165ec8c382ec7097eb63814794cb
v1adem <v1adem@ukr.net>
2023-02-22 01:00:10 +0200

    Refactor movie schema to figma req

--- database/schemes/movie.js
+++ database/schemes/movie.js
@@ -4,13 +4,21 @@ const { float } = require("webidl-conversions");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  title: String,
-  author: String,
-  description: String,
-  //genre: String,
-  //time: String, //?type Date
-  //rating: Number,
-  //image: Image
+  id: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+  description: {type: String, required: true},
+  type: {
+    type: String,
+    enum: ["Movie", "TV show"],
+    required: true,
+  },
+  genres: [String],
+  tagline: String,
+  poster: String,
+  backdrop: String,
+  date: Date,
+  rating: Number,
+  runtime: Number
 });
 
 const Movie = mongoose.model("Movie", schema);
