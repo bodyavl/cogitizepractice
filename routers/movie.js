d3267ff6211c7df797906c35e33ec06d60bb3c2e
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-02-21 20:49:03 +0200

    refactor fetching

--- routers/movie.js
+++ routers/movie.js
@@ -75,7 +75,7 @@ const FETCHINGDELAY = 5000;
 const iterationCount = 50;
 async function addMoviesToDatabase(pageIteration = 1) {
   if (pageIteration > 20000) return;
-  for (let i = 1; i < pageIteration + iterationCount ; i++) {
+  for (let i = pageIteration; i < pageIteration + iterationCount ; i++) {
     const movieRes = await axios.get(
       "https://api.themoviedb.org/3/discover/movie",
       {
