c0c195c03d9bf660a195834c17fcfdccc6fe6117
v1adem <v1adem@ukr.net>
2023-02-22 04:10:22 +0200

    refactored cleaning the db of movies /list upgraded

--- routes/movie.js
+++ routes/movie.js
@@ -5,42 +5,27 @@ const axios = require("axios").default;
 
 const Movie = require("../database/schemes/movie");
 
-// router.get("/axios/:id", async (req, res, next) => {
-//   try {
-//     const movieId = Number(req.params.id);
-//     const movie = await axios.get(
-//       `https://api.themoviedb.org/3/movie/${movieId}`,
-//       {
-//         params: {
-//           api_key: process.env.TMDB_API_KEY,
-//         },
-//       }
-//     );
-//     res.json(movie);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+const { shuffle } = require("../shuffle");
 
 router.get("/list", async (req, res, next) => {
   try {
-    const moviesList = await Movie.find({});
-    //console.log(moviesList);
-    res.json(moviesList);
+    const moviesList = await Movie.find().select("_id title genres");
+    const shuffledMoviesList = shuffle(moviesList);
+    res.status(200).json(shuffledMoviesList.slice(0, 10));
   } catch (error) {
     next(error);
   }
 });
 
-// Delete all notes in the linked DB
+// Delete all movie-notes in the linked DB
 router.get("/cleanDB", async (req, res, next) => {
   try {
     const moviesList = await Movie.find({});
     for (let movie of moviesList) {
-      await Movie.findByIdAndDelete(movie.id);
+      await Movie.findByIdAndDelete(movie._id);
     }
     res.sendStatus(200);
-    console.log("DataBase has been cleaned. It's empty!");
+    console.log("DB of movies has been cleaned. It's empty!");
   } catch (error) {
     next(error);
   }
@@ -89,7 +74,7 @@ router.get("/TMDB/fill", async (req, res, next) => {
 const FETCHINGDELAY = 5000;
 const iterationCount = 50;
 async function addMoviesToDB(pageIteration = 1) {
-  if (pageIteration > 10000) return;
+  if (pageIteration > 1000) return;
   for (let i = pageIteration; i < pageIteration + iterationCount; i++) {
     const moviesRaw = await axios.get(
       "https://api.themoviedb.org/3/discover/movie",
