93651de7adf1afb83db1115c079fcdc30ba41600
Skyba123 <skyba.serhi@gmail.com>
2023-02-22 06:47:14 +0200

    changed

--- index.js
+++ index.js
@@ -10,12 +10,46 @@ app.use(bodyParser.json({ type: 'application/json' }));
 const port = 3000;
  
 function errorTamer (res,req,error){
-res.header("Content-Type", "application/json");
+res.header("Content-Rype", "application/json");
 console.log("Error:", error.message);
 res.status(500).send(error.message);
 }
-
-app.use("/movie", movieRouter);
+app.get('/movie', async (req, res, next) => {
+   const movie = await Movie.find();
+   res.json(movie);
+});
+app.get("/getMovieById/:_id", async(req,res,next) => {
+  try{
+    const {_id} = req.params;
+    const movie = await Movie.findById(_id);
+  
+    if (!movie){
+      throw new Error("Not Found");
+    } else {
+      res.json(movie);
+    }
+  } catch(error){
+    next(error)
+  }
+  
+});
+app.post('/createMovie', async (req, res, next) => {
+  try{
+    const {title, author, description, country, rating, genre} = req.body;
+    const movie =  await Movie.create ({
+      title,
+      author,
+      description,
+      country,
+      rating, 
+      genre
+    });
+    console.log("Movie created :", movie);
+    res.json(movie);
+  } catch (error) {
+    next(error);
+  }
+  });
   app.use(errorTamer);
 
 app.listen(port, () => {
