776a4b04474151a8cc02d1b32cb8c0d0ce17fb70
Skyba123 <skyba.serhi@gmail.com>
2023-02-22 07:51:16 +0200

    changed

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 //mongodb+srv://Skyba:<password>@cogitizepractice.9lnnjje.mongodb.net/?retryWrites=true&w=majority
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 mongoose.set('strictQuery', true);
 function connect() {
     try {
