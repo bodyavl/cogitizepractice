2be58ab0e0cb63ab7ea3e55e30997cdd7bf75d1c
Skyba123 <skyba.serhi@gmail.com>
2023-02-25 22:03:30 +0200

    changed 8

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-
-const cors = require("cors");
-const dotenv = require("dotenv");
+const dotenv=require("dotenv");
 dotenv.config();
+const cors = require("cors");
 const express = require('express');
 const bodyParser = require('body-parser');
-const movieRouter = require("./routers/movie");
-const database = require("./database/index");
+const database = require ("./database");
+const { router, runBackgroundFetching } = require("./routers/movie");
 
-const app = express()
-app.use(cors());
-app.use(bodyParser.json({ type: 'application/json' }))
 
-const port = process.env.PORT;
-
-function errorHandler(error, req, res, next) {
-  res.header("Content-Type", "application/json");
-  console.log("Error occered:", error.message);
-  res.status(500).send(error.message);
-
+const app = express();
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use("/movie", router);
+ 
+function errorTamer (res,req,error){
+res.header("Content-Type", "application/json");
+console.log("Error:", error.message);
+res.status(500).send(error.message);
 }
 
-app.use("/movie", movieRouter);
+app.use(cors( {credentials: true, origin:true} ));
+  app.use(errorTamer);
 
-app.use(errorHandler);
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(process.env.node_env, "started")
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
+  runBackgroundFetching();
 });
-
-function testDelay(arg) {
-  console.log("arg ", arg);
-}
--- more/random.js
+++
@@ -1,7 +0,0 @@
-
-function getRandomArbitrary(min, max) {
-    max += 1;
-    return Math.random() * (max - min) + min;
-}
-
-module.exports = getRandomArbitrary;
