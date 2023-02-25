eaa623cff02aa759133a84aea75c13ce9de0a975
Skyba123 <skyba.serhi@gmail.com>
2023-02-25 19:35:42 +0200

    changed

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const app = express();
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use("/movie", router);
  
-function errorTamer (res,req,error){
-res.header("Content-Type", "application/json");
-console.log("Error:", error.message);
-res.status(500).send(error.message);
+function errorHandler(error, req, res, next) {
+  res.header("Content-Type", "application/json");
+  console.log("Error occured: ", error.message);
+  res.status(500).send(error.message);
 }
 
 
-  app.use(errorTamer);
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("On port", port);
   runBackgroundFetching();
 });
