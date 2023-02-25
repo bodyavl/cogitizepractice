f89f47dc643f94a1cc9c8aee17cc223a3eb035b7
Skyba123 <skyba.serhi@gmail.com>
2023-02-25 22:14:34 +0200

    changed 9

--- database/index.js
+++ database/index.js
@@ -1,18 +1,18 @@
 //mongodb+srv://Skyba:<password>@cogitizepractice.9lnnjje.mongodb.net/?retryWrites=true&w=majority
+
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-dotenv.config();
-mongoose.set('strictQuery', true);
+
 function connect() {
     try {
-        mongoose.connect (process.env.DATABASE_URL,{
+        mongoose.connect(process.env.DATABASE_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
-        console.log("Connecting to MongoDB");
+        })
+        console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error occured:", error.message);
+        console.log("Error occured", error.message);
     }
 }
+
 connect();
 
--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const dotenv=require("dotenv");
-dotenv.config();
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const database = require ("./database");
-const { router, runBackgroundFetching } = require("./routers/movie");
+const movieRouter = require("./routers/movie");
+const database = require("./database/index");
 
+const app = express()
+app.use(cors());
+app.use(bodyParser.json({ type: 'application/json' }))
 
-const app = express();
-app.use(bodyParser.json({ type: 'application/json' }));
-app.use("/movie", router);
- 
-function errorTamer (res,req,error){
-res.header("Content-Type", "application/json");
-console.log("Error:", error.message);
-res.status(500).send(error.message);
+const port = process.env.PORT;
+
+function errorHandler(error, req, res, next) {
+  res.header("Content-Type", "application/json");
+  console.log("Error occered:", error.message);
+  res.status(500).send(error.message);
+
 }
 
-app.use(cors( {credentials: true, origin:true} ));
-  app.use(errorTamer);
+app.use("/movie", movieRouter);
 
-const port = process.env.PORT || 3000;
+app.use(errorHandler);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-  runBackgroundFetching();
+  console.log(process.env.node_env, "started")
+  console.log(`Example app listening on port ${port}`)
 });
+
+function testDelay(arg) {
+  console.log("arg ", arg);
+}
