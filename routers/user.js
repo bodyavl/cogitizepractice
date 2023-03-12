b9eff5e69215b477b904d0a4b3b80e216ed4e165
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-03-12 11:31:50 +0200

    logging header

--- routers/user.js
+++ routers/user.js
@@ -98,6 +98,7 @@ router.get('/stats', authToken, async (req, res, next) => {
 
 function authToken(req, res, next) {
   const authHeader = req.headers['authorization'];
+  console.log(authHeader);
   const token = authHeader.split(' ')[1];
   if(!token) return res.sendStatus(401);
 
