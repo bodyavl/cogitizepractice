849eb0ad403b99ebf1ddd4177e9e24f02ec00ff4
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-03-21 13:54:57 +0200

    return specific fields in movie/list

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
 const Movie = require('./db/models/movie');
 async function getRandomMovies(options, limit = 8){
-  return await Movie.aggregate([{ $match: { ...options } }, { $sample: { size: limit } }]);
+  return await Movie.aggregate([{ $match: { ...options } }, { $sample: { size: limit } }, { $project: { type: 1, title: 1, poster: 1, rating: 1, genres: 1 }  }]);
 }
+// .select("_id type title poster rating genres")
 module.exports = {
   getRandomMovies
 };
